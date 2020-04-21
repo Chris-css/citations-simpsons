e79f5e976a302a23b12d4b5ba8a4a608561f0dce
Chris-css <christelle.savoca@gmail.com>
2020-04-21 22:38:09 +0200

    quête citations simpsons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import QuoteCard from './components/QuoteCard';
+import axios from 'axios';
 
-function App() {
+const quotes = {
+  quote: "Inflammable means flammable? What a country!",
+  character: "Dr. Nick",
+  image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNickRiviera.png?1497567511084",
+  characterDirection: "Right",
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      card: quotes
+    };
+    this.getCard = this.getCard.bind(this);
+  }
+
+getCard() {
+  
+  axios
+    .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+    .then(response => response.data)
+    .then(data => {
+      console.log(data)
+      this.setState({
+        card: data[0]
+      });
+    });
+}
+
+render () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QuoteCard card={this.state.card} />
+      <button type="button" onClick={this.getCard}>Get quote</button>
     </div>
   );
+}
 }
 
 export default App;
